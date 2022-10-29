568be24fb8131cd766dc1b56fd482fe978ee394c
Keosha Young <kyoung1012@gmail.com>
2022-10-29 19:07:49 -0400

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,17 @@
+print("Marvel Movie Character Creator")
+print("--")
+spiderMan = input("Do you like 'hanging around'?")
+if spiderMan == "no":
+    print("Then you're not Spider-man")
+else:
+    print("You could be Spider-man")
+korg = input("Do you have a 'gravelly' voice?")
+if korg == "no":
+    print("Aww, then you're not Korg!")
+else:
+    print("You could be Korg!")
+marvelous = input("Do you often feel 'Marvelous'?")
+if marvelous == "yes":
+    print("Aha! You're Captain Marvel! Hi!")
+else:
+    print("I have no idea who you are!")
